cdc7b76fb04311e09fc09b459054f295571065d5
HamGildo <ham5477@naver.com>
2021-12-07 15:06:17 +0900

    회원 정보 수정 라우터, 회원 정보 검색 라우터 추가

--- routes/member.js
+++ routes/member.js
@@ -75,6 +75,70 @@ router.post("/Login", function (request, response) {
     //conn.end();
 });
 
+// 회원 정보를 가져오는 라우터
+router.post("/MemberInfo", function (request, response) {
+    console.log(request.body);
+    let mem_id = request.body.mem_id;
+
+    let sql = "select * from member where mem_id = ?";
+    conn.query(sql, [mem_id], function (err, rows) {
+        if (!err) {
+            console.log(rows);
+            let arr = new Array(); 
+            let data = new Object();
+
+            // id로 요청하면 어차피 row는 1개만 나옴 (id는 유일값)
+            data.nick = rows[0].nick;
+            data.birth = rows[0].birth;
+            data.mem_image = rows[0].mem_image;
+            // 돌려주고 싶은 자료들 보내주면 됨
+
+            arr.push(data);
+            
+
+            let jsonData = JSON.stringify(arr);
+        
+            console.log(jsonData);
+            response.send(jsonData);
+             
+        } else {
+            console.log(err);
+        }
+    });
+
+});
+
+// 회원수정라우터
+router.post("/Modify", function (request, response) {
+    console.log(request.body);
+
+    let nick = request.body.nick;
+    let pw = request.body.pw;
+    let birth = request.body.birth;
+    let mem_image = request.body.mem_image;
+    let mem_id = request.body.id;
+
+    let sql = "update member set nick = ?, pw = ?, birth = ?, mem_image = ? where mem_id = ?";
+    conn.query(sql, [nick, pw, birth, mem_image, mem_id], function (err, rows) {
+        if (!err) {
+            console.log(rows);
+            let arr = new Array();
+            let data = new Object();
+            data.status = "200";
+            arr.push(data);
+            let jsonData = JSON.stringify(arr);
+            console.log(jsonData);
+            response.send(jsonData);
+
+        } else {
+            console.log(err);
+        }
+    });
+    //sql 명령 실행
+    //conn.end();
+});
+
+
 
 
 
