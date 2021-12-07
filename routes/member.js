1f8ebd88bd8369757f150a12383e442721672802
HamGildo <ham5477@naver.com>
2021-12-07 12:37:32 +0900

    회원가입 라우터 추가

--- routes/member.js
+++ routes/member.js
@@ -2,6 +2,37 @@ const express = require("express");
 const router = express.Router();
 const conn = require("../config/DBConn.js");
 
+//회원가입 라우터
+router.post("/Sign", function (request, response) {
+    console.log(request.body);
+
+    let mem_id = request.body.mem_id;
+    let pw = request.body.pw;
+    let nick = request.body.nick;
+    let birth = request.body.birth;
+    let gender = request.body.gender;
+
+    let sql = "insert into member(mem_id, pw, nick, birth, gender) values(?, ?, ?, ?, ?)";
+    conn.query(sql, [mem_id, pw, nick, birth, gender], function (err, rows) {
+        if (!err) {
+            console.log(rows);
+            
+            let arr = new Array();
+            let data = new Object();
+            data.status = "200";
+            arr.push(data);
+            let jsonData = JSON.stringify(arr);
+            console.log(jsonData);
+            response.send(jsonData);
+
+        } else {
+            console.log(err);
+        }
+    });
+    //sql 명령 실행
+    //conn.end();
+});
+
 
 //로그인 라우터
 router.post("/Login", function (request, response) {
@@ -43,5 +74,9 @@ router.post("/Login", function (request, response) {
     //sql 명령 실행
     //conn.end();
 });
+
+
+
+
 
 module.exports = router;
