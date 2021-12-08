92b5b04108f699c8c2654be40f7d9b795372d392
HamGildo <ham5477@naver.com>
2021-12-08 14:10:51 +0900

    내 미션 목록 확인 라우터 추가

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
 const memberRouter = require("./routes/member");
+const missionRouter = require("./routes/mission");
 
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use('/Member', memberRouter);
+app.use('/Mission', missionRouter);
 
 app.listen(3003);
 // 3003 -> server, 3306 -> Mysql
---
+++ routes/mission.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const conn = require("../config/DBConn.js");
+
+
+//내 미션 목록 확인 라우터
+router.post("/MyMission", function (request, response) {
+    console.log(request.body);
+
+    let mem_id = request.body.mem_id;
+
+    let sql = `select lo.location_name, sum(succ) as star, badge 
+            from mission_member mm join mission_bank mb 
+            on mm.mission_id = mb.mission_id 
+            join location lo 
+            on mb.location_name = lo.location_name 
+            where mm.mem_id = ? group by (location_name)`
+    conn.query(sql, [mem_id], function (err, rows) {
+        if (!err) {
+            console.log(rows);
+            
+            let arr = new Array();
+            for(let i = 0; i  <rows.length; i++) {
+                let data = new Object();
+                data.location_name = rows[i].location_name;
+                data.star = rows[i].star;
+                data.badge = rows[i].badge;
+
+                arr.push(data);
+            }
+            let jsonData = JSON.stringify(arr);
+            console.log(jsonData);
+            response.send(jsonData);
+
+        } else {
+            console.log(err);
+        }
+    });
+    //sql 명령 실행
+    //conn.end();
+});
+
+
+module.exports = router;
